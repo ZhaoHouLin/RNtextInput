8f4ab0b6d133e234aab36a3d3a7a9401cc913f68
硬體廠商維修2 <fixer2@cdc.gov.tw>
2020-03-26 19:56:42 +0800

    ui修改

--- App.js
+++ App.js
@@ -6,54 +6,55 @@ export default function App() {
   const [validCode, setValidCode] = useState('')
   const [confirmCode ,setConfirmCode] = useState('')
   const [answerCode, setanswerCode] = useState('')
+  const [colorState, setColorState] = useState('')
 
   const validCodeChange = (text) => {
     if(text === validCode) {
       // setValidCode(text)
       setConfirmCode(text)
-      setanswerCode('輸入成功！')
+      setanswerCode('輸入正確！')
+      setColorState(styles.rightResult)
+      
     } else {
       // setValidCode(text)
       setConfirmCode(text)
-      setanswerCode('手機輸入錯誤！')
+      setanswerCode('輸入錯誤！')
+      setColorState(styles.wrongResult)
+      
     }
   }
   
-  const tooShort = {
-    enabled: true,
-    label: 'Too short',
-    labelColor: 'red'
-  }
-
   return (
     <View style={styles.container}>
-      <Text>請輸入您的手機號碼</Text>
-      <TextInput
-        style={styles.textInputStyle}
-        onChangeText={(text)=>{setValidCode(text)}}
-        value={validCode}
-        maxLength={10}
-        placeholder='please type numbers'
-        keyboardType={'numeric'}
-        secureTextEntry={true}
-        editable={true}
-        autoFocus={false}
-      />
-      <Text>再次輸入您的手機號碼</Text>
-      <TextInput
-        style={styles.textInputStyle}
-        onChangeText={validCodeChange}
-        value={confirmCode}
-        maxLength={10}
-        placeholder='please type numbers'
-        keyboardType={'numeric'}
-        secureTextEntry={true}
-        editable={true}
-        autoFocus={false}
-      />
-      <Text>再次輸入的手機號碼是：</Text>
-      <Text>{confirmCode}</Text>
-      <Text>{answerCode}</Text>
+      <View style={styles.uiStyle}>
+        <Text style={styles.textStyle}>請輸入您的手機號碼:</Text>
+        <TextInput
+          style={styles.textInputStyle}
+          onChangeText={(text)=>{setValidCode(text)}}
+          value={validCode}
+          maxLength={10}
+          placeholder='please type numbers'
+          keyboardType={'numeric'}
+          secureTextEntry={true}
+          editable={true}
+          autoFocus={false}
+        />
+        <Text style={styles.textStyle}>再次輸入您的手機號碼:</Text>
+        <TextInput
+          style={styles.textInputStyle}
+          onChangeText={validCodeChange}
+          value={confirmCode}
+          maxLength={10}
+          placeholder='please type numbers'
+          keyboardType={'numeric'}
+          secureTextEntry={true}
+          editable={true}
+          autoFocus={false}
+        />
+        <Text style={styles.textStyle}>再次輸入的手機號碼是：</Text>
+        <Text style={[styles.rightResult,{color:'#222'}]}>{confirmCode}</Text>
+        <Text style={colorState}>{answerCode}</Text>
+      </View>
 
     </View>
   );
@@ -66,16 +67,55 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
   textInputStyle: {
     height: 50, 
     width: 300, 
     borderRadius: 16, 
-    borderColor: 'darkgray', 
-    borderWidth: 5, 
-    backgroundColor: 'gray', 
-    color: 'white', 
-    fontSize: 28, 
+    borderColor: '#222', 
+    borderWidth: 2, 
+    backgroundColor: 'white', 
+    color: '#222', 
+    fontSize: 24, 
     textAlign: 'center',
     margin: 16 
-  }
+  },
+
+  textStyle: {
+    color: 'white',
+    fontSize: 28,
+    textAlign: 'center',
+  },
+
+  rightResult: {
+    width: 'auto',
+    height: '28px',
+    marginTop: 8,
+    color:'green',
+    fontSize: 24,
+    textAlign: 'center',
+  },
+
+  wrongResult: {
+    width: 'auto',
+    height: '28px',
+    marginTop: 8,
+    color:'red',
+    fontSize: 24,
+    textAlign: 'center',
+  },
+
+  uiStyle: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 'auto',
+    height: 'auto', 
+    backgroundColor: 'orange',
+    boxShadow: '8px 8px 16px rgba(0,0,0,0.5)',
+    padding: 32,
+    borderRadius: 16,
+    color: 'white', 
+  },
+
+
 });
